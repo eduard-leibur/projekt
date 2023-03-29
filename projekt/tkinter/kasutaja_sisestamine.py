fa7587fc6e81a61061eb3854f7c9dded89d88a65
eduard-leibur <eduard.leibur@gmail.com>
2023-03-29 15:01:41 +0300

    29.03 tunnis

--- projekt/tkinter/kasutaja_sisestamine.py
+++ projekt/tkinter/kasutaja_sisestamine.py
@@ -11,42 +11,60 @@ def kinnita():  # "Kinnita" nuppu vajutades
         andmed = json.load(fail)
 
     if kasutajanimi == "":
-        print("Kasutajanimi puudu")
-    if kasutajanimi in andmed:
-        if andmed[kasutajanimi] == parool:
-            print("Kasutaja sisse logitud.")
-            tkinter.messagebox.showinfo(title=None, message="Korras")
-        else:
-            tkinter.messagebox.showerror(title=None, message="Vale parool!")
+        print("Kasutajanimi puudu")             # info konsooli
+        tkinter.messagebox.showwarning(title=None, message="Sisestage kasutajanimi")
+    elif parool == "":
+        print("Parool puudu")                   # info konsooli
+        tkinter.messagebox.showwarning(title=None, message="Sisestage parool")
     else:
-        tkinter.messagebox.showwarning(title=None, message="Sellist kasutajat pole!")
+        if kasutajanimi in andmed:
+            if andmed[kasutajanimi] == parool:
+                print("Kasutaja", kasutajanimi, "sisse logitud.")  # info konsooli
+                sõnum = "Kasutaja " + kasutajanimi + " sisse logitud."
+                tkinter.messagebox.showinfo(title=None, message=sõnum)
+            else:
+                tkinter.messagebox.showerror(title=None, message="Vale parool!")
+        else:
+            tkinter.messagebox.showwarning(title=None, message="Sellist kasutajat pole!")
 
 
-def registreeri():
+def registreeri():  # "Registreeri" nuppu vajutades
     kasutajanimi = kasutajanimi_sisestus.get()
     parool = parool_sisestus.get()
 
-    with open("kasutajate andmed.json", "r") as fail_sisse:
-        andmed = json.load(fail_sisse)
+    if kasutajanimi.isascii():
+        if kasutajanimi.islower():
+            if len(parool) >= 3:
+                with open("kasutajate andmed.json", "r") as fail_sisse:
+                    andmed = json.load(fail_sisse)  # olemasolevate kasutajate lugemine
+                andmed[kasutajanimi] = parool  # uue kasutaja lisamine
+                with open("kasutajate andmed.json", "w") as fail_välja:
+                    json.dump(andmed, fail_välja)  # sõnastiku koos uue kasutajaga salvestamine
 
-    andmed[kasutajanimi] = parool
-
-    with open("kasutajate andmed.json", "w") as fail_välja:
-        json.dump(andmed, fail_välja)
-    print("Kasutaja", kasutajanimi, "registreeritud.")
-    sõnum = "Kasutaja " + kasutajanimi + " registreeritud."
-    tkinter.messagebox.showinfo(title=None, message=sõnum)
+                print("Kasutaja", kasutajanimi, "registreeritud.")  # info konsooli
+                sõnum = "Kasutaja " + kasutajanimi + " registreeritud."
+                tkinter.messagebox.showinfo(title=None, message=sõnum)
+            else:
+                tkinter.messagebox.showerror(title="Viga paroolis",
+                                             message="Parool peab koosnema vähemalt kolmest tähemärgist või numbrist!")
+        else:
+            tkinter.messagebox.showerror(title="Viga kasutajanimes",
+                                         message="Kasutajanimi peab olema ainult väiketähtedes!")
+    else:
+        tkinter.messagebox.showerror(title="Viga kasutajanimes",
+                                     message="Kasutajanimi ei tohi sisaldada täpitähti!")
 
 
 aken = tkinter.Tk()
 aken.title("Sisse logimine")
+
 laius = 400
 kõrgus = 300
 ekraani_laius = aken.winfo_screenwidth()
 ekraani_kõrgus = aken.winfo_screenheight()
 x = (ekraani_laius/2) - (laius/2)
 y = (ekraani_kõrgus/2) - (kõrgus/2)
-aken.geometry("%dx%d+%d+%d" % (laius, kõrgus, x, y))
+aken.geometry("%dx%d+%d+%d" % (laius, kõrgus, x, y))    # akna ekraani keskele sättimine
 
 kasutajanimi_sisestus = tkinter.StringVar()
 parool_sisestus = tkinter.StringVar()
--- projekt/tkinter/main.py
+++ projekt/tkinter/main.py
@@ -1,16 +1,23 @@
 import tkinter
+import os
+
+
+def kasutaja_sisestamine():
+    os.chdir("C:\\Users\\eduard.leibur\\Downloads\\projekt\\projekt\\tkinter")
+    os.system("python kasutaja_sisestamine.py")
+
 
 avakuva = tkinter.Tk()
 avakuva.title("Avakuva")
-laius = 400
-kõrgus = 300
+laius = 500
+kõrgus = 400
 ekraani_laius = avakuva.winfo_screenwidth()
 ekraani_kõrgus = avakuva.winfo_screenheight()
 x = (ekraani_laius/2) - (laius/2)
 y = (ekraani_kõrgus/2) - (kõrgus/2)
 avakuva.geometry('%dx%d+%d+%d' % (laius, kõrgus, x, y))
 
-testnupp = tkinter.Button(avakuva, text="Test", command=lambda: print("Test!"))
+testnupp = tkinter.Button(avakuva, text="Sisenemine", command=kasutaja_sisestamine)
 testnupp.pack()
 
 väljumisnupp = tkinter.Button(avakuva, text="Välju", command=lambda: quit(1))
