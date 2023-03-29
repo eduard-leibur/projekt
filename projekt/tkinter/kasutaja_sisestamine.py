0b9dc962bec628226df98e9b58da3d4f99d39120
eduard-leibur <eduard.leibur@gmail.com>
2023-03-29 14:29:26 +0300

    29.03 tunnis

--- projekt/tkinter/kasutaja_sisestamine.py
+++ projekt/tkinter/kasutaja_sisestamine.py
@@ -14,12 +14,28 @@ def kinnita():  # "Kinnita" nuppu vajutades
         print("Kasutajanimi puudu")
     if kasutajanimi in andmed:
         if andmed[kasutajanimi] == parool:
-            print("Korras!")
+            print("Kasutaja sisse logitud.")
             tkinter.messagebox.showinfo(title=None, message="Korras")
         else:
             tkinter.messagebox.showerror(title=None, message="Vale parool!")
     else:
         tkinter.messagebox.showwarning(title=None, message="Sellist kasutajat pole!")
+
+
+def registreeri():
+    kasutajanimi = kasutajanimi_sisestus.get()
+    parool = parool_sisestus.get()
+
+    with open("kasutajate andmed.json", "r") as fail_sisse:
+        andmed = json.load(fail_sisse)
+
+    andmed[kasutajanimi] = parool
+
+    with open("kasutajate andmed.json", "w") as fail_välja:
+        json.dump(andmed, fail_välja)
+    print("Kasutaja", kasutajanimi, "registreeritud.")
+    sõnum = "Kasutaja " + kasutajanimi + " registreeritud."
+    tkinter.messagebox.showinfo(title=None, message=sõnum)
 
 
 aken = tkinter.Tk()
@@ -41,13 +57,17 @@ kasutajanime_kast = tkinter.Entry(aken, textvariable=kasutajanimi_sisestus, font
 parooli_kast = tkinter.Entry(aken, textvariable=parool_sisestus, font=("calibre", 12))
 
 väljumisnupp = tkinter.Button(aken, text="Välju", command=lambda: quit(1))     # 1 - nupust väljumine
-kinnitus = tkinter.Button(aken, text="Kinnita", command=kinnita)
+sisse_logimine = tkinter.Button(aken, text="Logi sisse", command=kinnita)
+registreerimine = tkinter.Button(aken, text="Registreeri", command=registreeri)
+
 
 kasutajanime_silt.grid(row=0, column=0)
 kasutajanime_kast.grid(row=0, column=1)
 parooli_silt.grid(row=1, column=0)
 parooli_kast.grid(row=1, column=1)
-kinnitus.grid(row=2, column=1)
+
+sisse_logimine.grid(row=2, column=1)
+registreerimine.grid(row=2, column=2)
 väljumisnupp.grid(row=3, column=1)
 
 aken.mainloop()
