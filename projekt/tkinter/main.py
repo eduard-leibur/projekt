5bca49667505b2f7923dc84da194876713ef8d3e
eduard-leibur <eduard.leibur@gmail.com>
2023-03-29 15:22:52 +0300

    29.03 tunnis

--- projekt/tkinter/main.py
+++ projekt/tkinter/main.py
@@ -5,6 +5,9 @@ import os
 def kasutaja_sisestamine():
     os.chdir("C:\\Users\\eduard.leibur\\Downloads\\projekt\\projekt\\tkinter")
     os.system("python kasutaja_sisestamine.py")
+    with open("kasutaja.txt", "r") as fail:
+        sisestatud_kasutaja = fail.read()
+    kasutaja_silt.config(text=sisestatud_kasutaja)
 
 
 avakuva = tkinter.Tk()
@@ -17,9 +20,9 @@ x = (ekraani_laius/2) - (laius/2)
 y = (ekraani_kõrgus/2) - (kõrgus/2)
 avakuva.geometry('%dx%d+%d+%d' % (laius, kõrgus, x, y))
 
+
 with open("kasutaja.txt", "r") as kasutaja_fail:
     kasutaja = kasutaja_fail.read()
-
 kasutaja_silt = tkinter.Label(avakuva, text=kasutaja)
 kasutaja_silt.pack()
 
