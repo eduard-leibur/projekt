fab3a6dc20f8a8f25d1da885ef421e9a8f4fbc37
eduard-leibur <eduard.leibur@outlook.com>
2023-04-28 13:30:53 +0300

    28.04 kodus; edusammud frame'dega

--- test.py
+++ test.py
@@ -1,3 +1,42 @@
-import subprocess
+import tkinter
+from funktsioonid.geomeetria import geomeetria_keskele
 
-subprocess.run(["python", "test2.py"])
+
+def soovikiri_leht():
+    soovikiri = tkinter.Frame(põhikuva)
+
+    pealkiri = tkinter.Label(soovikiri, text="Soovikiri", font=("Bold", 12), pady=20)
+    pealkiri.pack()
+
+    soovikiri.pack()
+    soovikiri.pack_propagate(False)
+    soovikiri.configure(width=1000, height=550)
+
+
+kuva = tkinter.Tk()
+kuva.title("Filmid")
+geomeetria_keskele(kuva, 1000, 700)
+
+with open("andmed/kasutaja.txt", "r") as kasutaja_fail:
+    kasutaja = kasutaja_fail.read()
+print(kasutaja + "'i filmid.")
+
+
+valikuriba = tkinter.Frame(kuva, bg="lightblue")
+valikuriba.pack_propagate(False)
+valikuriba.pack(side="top", fill="x")
+valikuriba.configure(width=1000, height=150)
+
+soovikiri_nupp = tkinter.Button(valikuriba, text="Soovikiri", command=lambda: soovikiri_leht())
+soovikiri_nupp.place(x=20, y=20)
+
+väljumisnupp = tkinter.Button(valikuriba, text="Välju", command=lambda: quit(1))     # 1 - nupust väljumine
+väljumisnupp.pack(side="right")
+
+
+põhikuva = tkinter.Frame(kuva, bg="lightgreen")
+põhikuva.pack(side="top", fill="both")
+põhikuva.pack_propagate(False)
+põhikuva.configure(width=1000, height=550)
+
+kuva.mainloop()
