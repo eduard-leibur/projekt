90f301035ab4986a84dce6a3930d3a1d2caae5f6
eduard-leibur <eduard.leibur@gmail.com>
2023-05-01 20:29:41 +0300

    05.01 ühikas vahelehed töötavad, vaja sisu ja funktsioonide struktureerimist teiste kategooriate jaoks

--- projekt/filmid.py
+++ projekt/filmid.py
@@ -2,6 +2,25 @@ import tkinter
 from funktsioonid.geomeetria import geomeetria_keskele
 
 
+def lehe_avamine(indikaator, leht):
+    peida_indikaatorid()
+    indikaator.configure(bg="blue")
+    tühjenda_põhikuva()
+    leht()
+
+
+def tühjenda_põhikuva():
+    for leht in põhikuva.winfo_children():
+        leht.destroy()
+
+
+def peida_indikaatorid():
+    soovikiri_indikaator.configure(bg=valikuriba_värv)
+    vaadatud_indikaator.configure(bg=valikuriba_värv)
+    nimekiri1_indikaator.configure(bg=valikuriba_värv)
+    nimekiri2_indikaator.configure(bg=valikuriba_värv)
+
+
 def leht_soovikiri():
     soovikiri = tkinter.Frame(põhikuva)
 
@@ -24,16 +43,26 @@ def leht_vaadatud():
     vaadatud_leht.configure(width=1000, height=550)
 
 
-def indikaator_esile(indikaator):
-    peida_indikaatorid()
-    indikaator.configure(bg="blue")
+def leht_nimekiri1():
+    vaadatud_leht = tkinter.Frame(põhikuva)
+
+    pealkiri = tkinter.Label(vaadatud_leht, text="Nimekiri 1", font=("Bold", 12), pady=20)
+    pealkiri.pack()
+
+    vaadatud_leht.pack()
+    vaadatud_leht.pack_propagate(False)
+    vaadatud_leht.configure(width=1000, height=550)
 
 
-def peida_indikaatorid():
-    soovikiri_indikaator.configure(bg=valikuriba_värv)
-    vaadatud_indikaator.configure(bg=valikuriba_värv)
-    nimekiri1_indikaator.configure(bg=valikuriba_värv)
-    nimekiri2_indikaator.configure(bg=valikuriba_värv)
+def leht_nimekiri2():
+    vaadatud_leht = tkinter.Frame(põhikuva)
+
+    pealkiri = tkinter.Label(vaadatud_leht, text="Nimekiri 2", font=("Bold", 12), pady=20)
+    pealkiri.pack()
+
+    vaadatud_leht.pack()
+    vaadatud_leht.pack_propagate(False)
+    vaadatud_leht.configure(width=1000, height=550)
 
 
 kuva = tkinter.Tk()
@@ -54,16 +83,20 @@ valikuriba.configure(width=1000, height=150)
 nuppude_vahe = 30
 nuppude_laius = -100
 
-soovikiri_nupp = tkinter.Button(valikuriba, text="Soovikiri", command=lambda: indikaator_esile(soovikiri_indikaator))
+soovikiri_nupp = tkinter.Button(valikuriba, text="Soovikiri",
+                                command=lambda: lehe_avamine(soovikiri_indikaator, leht_soovikiri))
 soovikiri_nupp.pack(side=tkinter.LEFT, padx=nuppude_vahe)
 
-vaadatud_nupp = tkinter.Button(valikuriba, text="Vaadatud", command=lambda: indikaator_esile(vaadatud_indikaator))
+vaadatud_nupp = tkinter.Button(valikuriba, text="Vaadatud",
+                               command=lambda: lehe_avamine(vaadatud_indikaator, leht_vaadatud))
 vaadatud_nupp.pack(side=tkinter.LEFT, padx=0)
 
-nimekiri1_nupp = tkinter.Button(valikuriba, text="Nimekiri 1", command=lambda: indikaator_esile(nimekiri1_indikaator))
+nimekiri1_nupp = tkinter.Button(valikuriba, text="Nimekiri 1",
+                                command=lambda: lehe_avamine(nimekiri1_indikaator, leht_nimekiri1))
 nimekiri1_nupp.pack(side=tkinter.LEFT, padx=nuppude_vahe)
 
-nimekiri2_nupp = tkinter.Button(valikuriba, text="Nimekiri 2", command=lambda: indikaator_esile(nimekiri2_indikaator))
+nimekiri2_nupp = tkinter.Button(valikuriba, text="Nimekiri 2",
+                                command=lambda: lehe_avamine(nimekiri2_indikaator, leht_nimekiri2))
 nimekiri2_nupp.pack(side=tkinter.LEFT, padx=0)
 valikuriba.update()
 
@@ -87,6 +120,10 @@ nimekiri2_indikaator.place(x=4 * nuppude_vahe + soovikiri_nupp.winfo_width() +
 
 väljumisnupp = tkinter.Button(valikuriba, text="Välju", command=lambda: quit(1))     # 1 - nupust väljumine
 väljumisnupp.pack(side=tkinter.RIGHT, padx=nuppude_vahe)
+
+kasutaja_sildi_tekst = "Kasutaja: " + kasutaja
+kasutaja_silt = tkinter.Label(valikuriba, text=kasutaja_sildi_tekst, font=("Bold", 12))
+kasutaja_silt.pack(side=tkinter.RIGHT)
 
 
 põhikuva = tkinter.Frame(kuva, bg="lightgreen")
